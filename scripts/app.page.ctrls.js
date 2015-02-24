f6ef82c84fdf44e7fc292f4b1fa25650d3e946fe
Luca <l.colangiuli@gmail.com>
2015-02-24 23:22:54 +0100

    inizio edit

--- scripts/app.page.ctrls.js
+++ scripts/app.page.ctrls.js
@@ -5,7 +5,7 @@
                 var originalContents, popupWin, printContents;
                 return printContents = document.getElementById("invoice").innerHTML, originalContents = document.body.innerHTML, popupWin = window.open(), popupWin.document.open(), popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="styles/main.css" /></head><body onload="window.print()">' + printContents + "</html>"), popupWin.document.close()
             }
-        }]).controller("budgetCtrl", ["$scope", "$filter","Categories", function($scope, $filter,Categories) {
+        }]).controller("budgetCtrl", ["$scope", "$filter","Categories","$modal", function($scope, $filter,Categories,$modal) {
             var init;
 
             Categories.getFull().success(function(data){
@@ -75,6 +75,30 @@
             $scope.numPerPage = $scope.numPerPageOpt[2];
             $scope.currentPage = 1;
             $scope.currentPagebudgets = [];
+
+            $scope.open = function() {
+                var modalInstance;
+                modalInstance = $modal.open({
+                    templateUrl: "editBudget.html"//,
+                   // controller: "ModalInstanceCtrl",
+                })
+            }
+
+            $scope.editBudget = function(objectId){
+                $scope.budget = {"name": "ciao"};
+                                var modalInstance;
+                modalInstance = $modal.open({
+                    templateUrl: "editBudget.html",
+                   // controller: "ModalInstanceCtrl",
+                   scope: $scope
+                })
+            }
+            $scope.deleteBudget = function(objectId){
+                
+            }
+            $scope.newBudget = function(){
+                
+            }
             /*(init = function() {
                 return $scope.search(), $scope.select($scope.currentPage)
             })();*/
